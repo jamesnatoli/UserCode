719065e29b9c6daf9ea2ecc54b799f8399198594
jamesnatoli <james.v.natoli@gmail.com>
2018-03-27 12:27:27 -0400

    Added rotated X and Y efficiency maps

--- TestBeam/slimanalysis.C
+++ TestBeam/slimanalysis.C
@@ -1,16 +1,14 @@
 #include "slimanalysis.h"
-#include "helper_funcs.c"
+#include "helper_funcs.C"
 
-void doAlignmentPlots(const char*
-                      dir="~/TB_Analysis_17/DATA/new_SLIM/",
-                      bool debug=false) {
+void doAlignmentPlots(bool debug, const char* dir) {
 
   TChain* chain = new TChain("slim");
   chain->Add(Form("%s/*_slim.root",dir));
 
-  // Plots without correction                                                                                                                   
-  const int NPLANES = 4; // uhm, technically it is something different...                                                                       
-  TH1F* hist[NPLANES]; // There will be four histograms                                                                                         
+  // Plots without correction
+  const int NPLANES = 4; // uhm, technically it is something different...
+  TH1F* hist[NPLANES]; // There will be four histograms
   const string var[NPLANES] = {
     "xC-xA",
     "yC-yA",
@@ -30,9 +28,9 @@ void doAlignmentPlots(const char*
     chain->Project(var[i].c_str(),var[i].c_str(),
                    Form("abs(%s)<30",var[i].c_str()));
     mean[i] = hist[i]->Fit("gaus","SQN")->GetParams()[1];
-    // really not best way: why re-doing fit? I just need to                                                                                    
-    // check if resolution of wire chambers are similar or not                                                                                  
-    // (if different, will need to include uncertainties in tracking fit                                                                        
+    // really not best way: why re-doing fit? I just need to
+    // check if resolution of wire chambers are similar or not
+    // (if different, will need to include uncertainties in tracking fit
     sigma[i] = hist[i]->Fit("gaus","SQN")->GetParams()[2];
 
     hist[i]->SetLineWidth(2);
@@ -73,10 +71,7 @@ void doAlignmentPlots(const char*
   }
 }
 
-void doMaps(const char*
-            dir="~/TB_Analysis_17/DATA/new_SLIM/",
-            bool debug=false) {
-
+void doMaps(bool debug, const char* dir) {
   // Identify channels we need to use
   struct channel {
     int chan;
@@ -85,19 +80,36 @@ void doMaps(const char*
     string name;
   };
 
+  bool eff, eff_rot, effX, effY, effX_rot, effY_rot;
+  effX = effY = effX_rot = effY_rot = !debug;
+  eff = eff_rot = !debug;
+
+  // Fill the Rotated Arrays
+  // This fills both the Theta array, and the fiducial arrays
+  fill_Rot_Array();
+
   // Let us define the histogram and book them
-  TH2F *hist_eff[NUMCHAN]; // 8 is the number of tiles; channels.size() == 8
+  TH2F *hist_eff[NUMCHAN]; 
   TH2F *hist_den[NUMCHAN];
-  TH1F *hist_effX[NUMCHAN]; // 8 is the number of tiles; channels.size() == 8
+  TH1F *hist_effX[NUMCHAN]; 
   TH1F *hist_denX[NUMCHAN];
-  TH1F *hist_effY[NUMCHAN]; // 8 is the number of tiles; channels.size() == 8
+  TH1F *hist_effY[NUMCHAN]; 
   TH1F *hist_denY[NUMCHAN];
 
+  // Rotated Maps
+  TH2F *hist_eff_rot[NUMCHAN];
+  TH2F *hist_den_rot[NUMCHAN];
+  TH1F *hist_effX_rot[NUMCHAN];
+  TH1F *hist_denX_rot[NUMCHAN];
+  TH1F *hist_effY_rot[NUMCHAN];
+  TH1F *hist_denY_rot[NUMCHAN];
+  
   if (channels.size()!= NUMCHAN) {
     cout << "Argh, something wrong!" << endl;
     return;
   }
-  for (unsigned int i=0;i<channels.size();++i) {
+
+  for (unsigned int i = 0; i < channels.size(); ++i) {
     // Horrible way to replace - with _ in the channel's name, so that we can print canvas to .C file
     // and obtain a usable macro
     hist_eff[i] = new TH2F(TString(Form("%s_eff",channels[i].name.c_str())).ReplaceAll("-","_").Data(),
@@ -120,6 +132,29 @@ void doMaps(const char*
                             "",400,-100,100);
     hist_effY[i]->Sumw2();
     hist_denY[i]->Sumw2();
+
+    // Rotated Plots
+    // 2D 
+    hist_eff_rot[i] = new TH2F(TString(Form("%s_eff_rot",channels[i].name.c_str())).ReplaceAll("-","_").Data(),
+                           "",350,-75,75,350,-75,75);
+    hist_den_rot[i] = new TH2F(TString(Form("%s_den_rot",channels[i].name.c_str())).ReplaceAll("-","_").Data(),
+                           "",350,-75,75,350,-75,75);
+    hist_eff_rot[i]->Sumw2();
+    hist_den_rot[i]->Sumw2();
+    // X Efficiency
+    hist_effX_rot[i] = new TH1F(TString(Form("%s_effX_rot",channels[i].name.c_str())).ReplaceAll("-","_").Data(),
+                            "",350,-100,100);
+    hist_denX_rot[i] = new TH1F(TString(Form("%s_denX_rot",channels[i].name.c_str())).ReplaceAll("-","_").Data(),
+                            "",350,-100,100);
+    hist_effX_rot[i]->Sumw2();
+    hist_denX_rot[i]->Sumw2();
+    // Y Efficiency
+    hist_effY_rot[i] = new TH1F(TString(Form("%s_effY_rot",channels[i].name.c_str())).ReplaceAll("-","_").Data(),
+                            "",400,-100,100);
+    hist_denY_rot[i] = new TH1F(TString(Form("%s_denY_rot",channels[i].name.c_str())).ReplaceAll("-","_").Data(),
+                            "",400,-100,100);
+    hist_effY_rot[i]->Sumw2();
+    hist_denY_rot[i]->Sumw2();
   }
 
   // Now we get the data
@@ -127,141 +162,243 @@ void doMaps(const char*
   chain->Add(Form("%s/*_slim.root",dir));
 
   // Get the branches I need:
-  vector<double> *xa = 0,*xb = 0,*xc = 0,*ya = 0,*yb = 0,*yc = 0;
-  chain->SetBranchAddress("xA",&xa);
-  //chain->SetBranchAddress("xB",&xb);
-  chain->SetBranchAddress("xC",&xc);
-  chain->SetBranchAddress("yA",&ya);
-  //chain->SetBranchAddress("yB",&yb);
-  chain->SetBranchAddress("yC",&yc);
+  vector<double> *xa = 0, *xc = 0,*ya = 0, *yc = 0;
+  chain->SetBranchAddress("xA", &xa);
+  chain->SetBranchAddress("xC", &xc);
+  chain->SetBranchAddress("yA", &ya);
+  chain->SetBranchAddress("yC", &yc);
 
   double pulse[NCH][NTS], ped[NCH];
-  chain->SetBranchAddress("pulse",&pulse);
-  chain->SetBranchAddress("ped",&ped);
+  chain->SetBranchAddress("pulse", &pulse);
+  chain->SetBranchAddress("ped", &ped);
 
   double intercept_X, slope_X;
   double intercept_Y, slope_Y;
-  chain->SetBranchAddress("interceptX",&intercept_X);
-  chain->SetBranchAddress("slopeX",&slope_X);
-  chain->SetBranchAddress("interceptY",&intercept_Y);
-  chain->SetBranchAddress("slopeY",&slope_Y);
+  chain->SetBranchAddress("interceptX", &intercept_X);
+  chain->SetBranchAddress("slopeX", &slope_X);
+  chain->SetBranchAddress("interceptY", &intercept_Y);
+  chain->SetBranchAddress("slopeY", &slope_Y);
 
   int run;
   chain->SetBranchAddress("run", &run);
 
-  for (unsigned int i=0;i<chain->GetEntries();++i) {
-    if (debug && i>1000)
+  // Event Loop
+  int counter = 0;
+  for (unsigned int j = 0; j < chain->GetEntries(); ++j) {
+    if (debug && j > 1000)
       continue;
-
+    
     // Do the alignment
-    chain->GetEntry(i);
-
+    chain->GetEntry(j);
+    
     double x_hit = intercept_X + z_ex*slope_X;
     double y_hit = intercept_Y + z_ex*slope_Y;
-
-    if (i%100000==0)
-      std::cout << "  Done with " << i << " / "
+    
+    if (j%100000==0)
+      std::cout << "  Done with " << j << " / "
                 << chain->GetEntriesFast() << " events\n";
     //cout << "(x,y) = (" << x_hit << "," << y_hit << ")\n";
-
+    
     // loop on the various channels
     for (unsigned int i = 0; i < channels.size(); ++i) {
-
-      // Use TS = 5, 6, 7, 8; remove 4 times the pedestal
-      double energy_ps =
-        pulse[channels[i].chan][5]+
-        pulse[channels[i].chan][6]+
-        pulse[channels[i].chan][7]+
-        pulse[channels[i].chan][8]-
-        4*ped[channels[i].chan];
-
+      // For Finger tiles
+      if (run < 3410 && i >= 3 && i <= 6)
+	continue;
+      
+      double x_hit_rot = rotate_Point(x_hit, y_hit, i, 'X');
+      double y_hit_rot = rotate_Point(x_hit, y_hit, i, 'Y');
+      
+      if ( isFiducial( i, x_hit, y_hit) ^ isRotFiducial( i, x_hit_rot, y_hit_rot)) {
+	cout << "ISSUE WITH" << "\n" 
+	     << "i = " << i << "\n"
+	     << "x_hit = " << x_hit << "\n"
+	     << "y_hit = " << y_hit << "\n"
+	     << endl;
+	counter++;
+      }
+      
+      double energy_ps = 0;
+      for (auto ts : TIMESLICES)
+        energy_ps+=pulse[channels[i].chan][ts];
+      energy_ps-=TIMESLICES.size()*ped[channels[i].chan];
+      
       if (energy_ps>25) {
         hist_eff[i]->Fill(x_hit,y_hit);
         hist_effX[i]->Fill(x_hit);
         hist_effY[i]->Fill(y_hit);
+	// Rotated
+	hist_eff_rot[i]->Fill(x_hit_rot, y_hit_rot);
+        hist_effX_rot[i]->Fill(x_hit_rot);
+        hist_effY_rot[i]->Fill(y_hit_rot);
       }
       hist_den[i]->Fill(x_hit,y_hit);
       hist_denX[i]->Fill(x_hit);
       hist_denY[i]->Fill(y_hit);
-
+      // Rotated
+      hist_den_rot[i]->Fill(x_hit_rot, y_hit_rot);
+      hist_denX_rot[i]->Fill(x_hit_rot);
+      hist_denY_rot[i]->Fill(y_hit_rot);
+      
     } // loop on channels
-
+    
   } // loop on events
+  cout << "counter = " << counter << endl;
 
-  // here I should plot the efficiency maps, adfter some beautification
+  // here I should plot the efficiency maps, after some beautification
   TCanvas *canv[NUMCHAN], *canvX[NUMCHAN], *canvY[NUMCHAN];
-
-  TLine* fid_line = new TLine();
-  fid_line->SetLineWidth(2);
-  fid_line->SetLineStyle(kDashed);
+  // Plot the rotated hists on these canvases
+  TCanvas *canv_rot[NUMCHAN], *canvX_rot[NUMCHAN], *canvY_rot[NUMCHAN];
+  
   for (unsigned int i = 0; i < channels.size(); ++i) {
-    hist_eff[i]->Divide(hist_eff[i],hist_den[i],1,1,"b");
-    hist_eff[i]->GetXaxis()->SetTitle("x [mm]");
-    hist_eff[i]->GetYaxis()->SetTitle("y [mm]");
-
-    canv[i] = new TCanvas(channels[i].name.c_str(),"",550,500);
-    canv[i]->SetRightMargin(canv[i]->GetLeftMargin());
-    hist_eff[i]->Draw("colz");
-    fid_line->DrawLine(fiducialX[i][0],fiducialY[i][0],
-                       fiducialX[i][1],fiducialY[i][1]);
-    fid_line->DrawLine(fiducialX[i][0],fiducialY[i][0],
-                       fiducialX[i][2],fiducialY[i][2]);
-    fid_line->DrawLine(fiducialX[i][3],fiducialY[i][3],
-                       fiducialX[i][1],fiducialY[i][1]);
-    fid_line->DrawLine(fiducialX[i][3],fiducialY[i][3],
-                       fiducialX[i][2],fiducialY[i][2]);
-
-    TLatex label;
-    label.SetNDC();
-    label.SetTextSize(0.05);
-    label.SetTextAlign(30);
-    //label.DrawLatex(0.92,0.875,entry[i].c_str());
-    label.DrawLatex(0.8,0.875,entry[i].c_str());
+    if (eff) {
+      // 2D Efficiency
+      // Set up line
+      TLine* fid_line = new TLine();
+      fid_line->SetLineWidth(2);
+      fid_line->SetLineStyle(kDashed);
+      
+      // Divide to get efficiency
+      hist_eff[i]->Divide(hist_eff[i],hist_den[i],1,1,"b");
+      // Set Axis
+      hist_eff[i]->GetXaxis()->SetTitle("x [mm]");
+      hist_eff[i]->GetYaxis()->SetTitle("y [mm]");
+      
+      canv[i] = new TCanvas(channels[i].name.c_str(),"",550,500);
+      canv[i]->SetRightMargin(canv[i]->GetLeftMargin());
+      hist_eff[i]->Draw("colz");
+      // Draw Lines
+      fid_line->DrawLine(fiducialX[i][0],fiducialY[i][0],
+			 fiducialX[i][1],fiducialY[i][1]);
+      fid_line->DrawLine(fiducialX[i][0],fiducialY[i][0],
+			 fiducialX[i][2],fiducialY[i][2]);
+      fid_line->DrawLine(fiducialX[i][3],fiducialY[i][3],
+			 fiducialX[i][1],fiducialY[i][1]);
+      fid_line->DrawLine(fiducialX[i][3],fiducialY[i][3],
+			 fiducialX[i][2],fiducialY[i][2]);
+      
+      TLatex label;
+      label.SetNDC();
+      label.SetTextSize(0.05);
+      label.SetTextAlign(30);
+      //label.DrawLatex(0.92,0.875,entry[i].c_str());
+      label.DrawLatex(0.8,0.875,entry[i].c_str());
+      
+      canv[i]->Print(Form("Original_Images/Efficiency_Maps_2D/efficiency_map_%s.png",channels[i].name.c_str()));
+      canv[i]->Print(Form("Original_Images/Efficiency_Maps_2D/efficiency_map_%s.C",channels[i].name.c_str()));
+    }
+    if (eff_rot) {
+      // 2D Rotated Efficiency
+      TLine* fid_line_rot = new TLine();
+      fid_line_rot -> SetLineWidth(2);
+      fid_line_rot -> SetLineStyle(kDashed);
+      
+      hist_eff_rot[i] -> Divide( hist_eff_rot[i], hist_den_rot[i],1,1,"b");
+      hist_eff_rot[i] -> GetXaxis() -> SetTitle("x [mm]");
+      hist_eff_rot[i] -> GetYaxis() -> SetTitle("y [mm]");
+      
+      canv_rot[i] = new TCanvas( (channels[i].name + "_rot").c_str(), "", 550, 500);
+      canv_rot[i] -> SetRightMargin(canv[i] -> GetLeftMargin());
+      hist_eff_rot[i] -> Draw("colz");
+      fid_line_rot -> DrawLine( rot_fiducialX[i][0], rot_fiducialY[i][0],
+			        rot_fiducialX[i][1], rot_fiducialY[i][1]);
+      fid_line_rot -> DrawLine( rot_fiducialX[i][0], rot_fiducialY[i][0],
+			        rot_fiducialX[i][2], rot_fiducialY[i][2]);
+      fid_line_rot -> DrawLine( rot_fiducialX[i][3], rot_fiducialY[i][3],
+			        rot_fiducialX[i][1], rot_fiducialY[i][1]);
+      fid_line_rot -> DrawLine( rot_fiducialX[i][3], rot_fiducialY[i][3],
+			        rot_fiducialX[i][2], rot_fiducialY[i][2]);
+      
+      TLatex label_rot;
+      label_rot.SetNDC();
+      label_rot.SetTextSize(0.05);
+      label_rot.SetTextAlign(30);
+      //label.DrawLatex(0.92,0.875,entry[i].c_str());
+      label_rot.DrawLatex(0.8,0.875, entry[i].c_str());
+      
+      canv_rot[i] -> Print(Form("Rotated_Images/Efficiency_Maps_2D/efficiency_map_rot%s.png", channels[i].name.c_str()));
+      canv_rot[i] -> Print(Form("Rotated_Images/Efficiency_Maps_2D/efficiency_map_rot%s.C", channels[i].name.c_str()));
+    }
     
-    canv[i]->Print(Form("Original_Images/Efficiency_Maps_2D/efficiency_map_%s.png",channels[i].name.c_str()));
-    canv[i]->Print(Form("Original_Images/Efficiency_Maps_2D/efficiency_map_%s.C",channels[i].name.c_str()));
-
-    hist_effX[i]->Divide(hist_effX[i],hist_denX[i],1,1,"b");
-    hist_effX[i]->GetXaxis()->SetTitle("x [mm]");
-    hist_effX[i]->GetYaxis()->SetTitle("Efficiency");
-
-    canvX[i] = new TCanvas(Form("effX_%s",channels[i].name.c_str()),
-                           "",500,500);
-    hist_effX[i]->Draw();
-
-    TLatex labelX;
-    labelX.SetNDC();
-    labelX.SetTextSize(0.05);
-    labelX.SetTextAlign(30);
-    labelX.DrawLatex(0.92,0.875,entry[i].c_str());
+    if (effX) {
+      hist_effX[i]->Divide(hist_effX[i],hist_denX[i],1,1,"b");
+      hist_effX[i]->GetXaxis()->SetTitle("x [mm]");
+      hist_effX[i]->GetYaxis()->SetTitle("Efficiency");
+      
+      canvX[i] = new TCanvas(Form("effX_%s", channels[i].name.c_str()),
+			     "", 500, 500);
+      hist_effX[i]->Draw();
+      
+      TLatex labelX;
+      labelX.SetNDC();
+      labelX.SetTextSize(0.05);
+      labelX.SetTextAlign(30);
+      labelX.DrawLatex(0.92,0.875,entry[i].c_str());
+      
+      canvX[i]->Print(Form("Original_Images/Efficiency_Maps_X/efficiency_x_%s.png", channels[i].name.c_str()));
+      canvX[i]->Print(Form("Original_Images/Efficiency_Maps_X/efficiency_x_%s.C", channels[i].name.c_str()));
+    }
     
-    canvX[i]->Print(Form("Original_Images/Efficiency_Maps_X/efficiency_x_%s.png", channels[i].name.c_str()));
-    canvX[i]->Print(Form("Original_Images/Efficiency_Maps_X/efficiency_x_%s.C", channels[i].name.c_str()));
-
-    hist_effY[i]->Divide(hist_effY[i],hist_denY[i],1,1,"b");
-    hist_effY[i]->GetXaxis()->SetTitle("y [mm]");
-    hist_effY[i]->GetYaxis()->SetTitle("Efficiency");
-
-    canvY[i] = new TCanvas(Form("effY_%s",channels[i].name.c_str()),
-                           "",500,500);
-    hist_effY[i]->Draw();
-
-    TLatex labelY;
-    labelY.SetNDC();
-    labelY.SetTextSize(0.05);
-    labelY.SetTextAlign(30);
-    labelY.DrawLatex(0.92,0.875,entry[i].c_str());
+    if (effX_rot) {
+      hist_effX_rot[i]->Divide(hist_effX_rot[i], hist_denX_rot[i], 1, 1, "b");
+      hist_effX_rot[i]->GetXaxis()->SetTitle("x [mm]");
+      hist_effX_rot[i]->GetYaxis()->SetTitle("Efficiency");
+      
+      canvX_rot[i] = new TCanvas(Form("effX_%s", (channels[i].name + "_rot").c_str()),
+			     "", 500, 500);
+      hist_effX_rot[i]->Draw();
+      
+      TLatex labelX_rot;
+      labelX_rot.SetNDC();
+      labelX_rot.SetTextSize(0.05);
+      labelX_rot.SetTextAlign(30);
+      labelX_rot.DrawLatex(0.92, 0.875, entry[i].c_str());
+      
+      canvX[i]->Print(Form("Rotated_Images/Efficiency_Maps_X/efficiency_x_rot%s.png", channels[i].name.c_str()));
+      canvX[i]->Print(Form("Rotated_Images/Efficiency_Maps_X/efficiency_x_rot%s.C", channels[i].name.c_str()));
+    }
     
-    canvY[i]->Print(Form("Original_Images/Efficiency_Maps_Y/efficiency_y_%s.png", channels[i].name.c_str()));
-    canvY[i]->Print(Form("Original_Images/Efficiency_Maps_Y/efficiency_y_%s.C", channels[i].name.c_str()));
+    if ( effY) {
+      hist_effY[i]->Divide(hist_effY[i],hist_denY[i],1,1,"b");
+      hist_effY[i]->GetXaxis()->SetTitle("y [mm]");
+      hist_effY[i]->GetYaxis()->SetTitle("Efficiency");
+      
+      canvY[i] = new TCanvas(Form("effY_%s",channels[i].name.c_str()),
+			     "",500,500);
+      hist_effY[i]->Draw();
+      
+      TLatex labelY;
+      labelY.SetNDC();
+      labelY.SetTextSize(0.05);
+      labelY.SetTextAlign(30);
+      labelY.DrawLatex(0.92,0.875,entry[i].c_str());
+      
+      canvY[i]->Print(Form("Original_Images/Efficiency_Maps_Y/efficiency_y_%s.png", channels[i].name.c_str()));
+      canvY[i]->Print(Form("Original_Images/Efficiency_Maps_Y/efficiency_y_%s.C", channels[i].name.c_str()));
+    }
+
+    if (effY_rot) {
+      hist_effY_rot[i]->Divide(hist_effY_rot[i], hist_denY_rot[i], 1, 1, "b");
+      hist_effY_rot[i]->GetXaxis()->SetTitle("y [mm]");
+      hist_effY_rot[i]->GetYaxis()->SetTitle("Efficiency");
+      
+      canvY_rot[i] = new TCanvas(Form("effY_%s", (channels[i].name + "_rot").c_str()),
+			     "",500,500);
+      hist_effY_rot[i]->Draw();
+      
+      TLatex labelY_rot;
+      labelY_rot.SetNDC();
+      labelY_rot.SetTextSize(0.05);
+      labelY_rot.SetTextAlign(30);
+      labelY_rot.DrawLatex(0.92,0.875, entry[i].c_str());
+      
+      canvY[i]->Print(Form("Rotated_Images/Efficiency_Maps_Y/efficiency_y_rot%s.png", channels[i].name.c_str()));
+      canvY[i]->Print(Form("Rotated_Images/Efficiency_Maps_Y/efficiency_y_rot%s.C", channels[i].name.c_str()));
+    }
   }
-
 }
 
 // Energy; time-slice
-void doEnergyTS(const char*
-                dir="~/TB_Analysis_17/DATA/new_SLIM/",
-                bool debug=false) {
+void doEnergyTS(bool debug, const char* dir) {
   // Make a TFile
   TFile *energy_hists = new TFile("energy_hists.root", "RECREATE");
   if (!energy_hists->IsOpen()) {
@@ -316,6 +453,7 @@ void doEnergyTS(const char*
     if (debug && i>1000)
       continue;
 
+
     // Do the alignment
     chain->GetEntry(i);
 
@@ -339,7 +477,7 @@ void doEnergyTS(const char*
         energy_ps+=pulse[channels[i].chan][ts];
       energy_ps-=TIMESLICES.size()*ped[channels[i].chan];
 
-      if(!isFiducial(i,x_hit,y_hit)) // THIS IS THE MOST IMPORTANT STEP EVERYTHING ELSE IS FLUFF
+      if(!isFiducial(i,x_hit,y_hit)) // THIS IS THE MOST IMPORTANT STEP 
         continue;
 
       hist_en[i]->Fill(energy_ps);
